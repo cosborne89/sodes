ff3c22717912a47c9a713ef13a2d2a11d0484370
Caleb Osborne <caleb.t.osborne+github@gmail.com>
2013-04-26 10:45:07 -0400

    Banner formatting and refactor
    Also added ckeditor to common knowledge

--- app/assets/javascripts/cosmetic.js
+++ app/assets/javascripts/cosmetic.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
 
 //Function to unhide/hide All/New when you mouse over a banner object
-$('.banner-object').mouseenter(function() {
-	$(this).children('.banner-object-child').toggle(400);
-	$(this).children('.banner-object-child').css("display", "inline-block");
-});
-$('.banner-object').mouseleave(function() {
-		$(this).children('.banner-object-child').toggle(400, function() {
-			$(this).children('.banner-object-child').css("display", "none");
+//$('.banner-object').mouseenter(function() {
+//	$(this).children('.banner-object-child').toggle(400);
+//	$(this).children('.banner-object-child').css("display", "inline-block");
+//});
+//$('.banner-object').mouseleave(function() {
+//		$(this).children('.banner-object-child').toggle(400, function() {
+//			$(this).children('.banner-object-child').css("display", "none");
+//		});
+//		
+//});
+$('.dropdown').each(function () {
+	$(this).parent().eq(0).hover(function () {
+		$('.dropdown:eq(0)', this).show();
+		}, function () {
+			$('.dropdown:eq(0)', this).hide();
 		});
-		
 });
 
 
